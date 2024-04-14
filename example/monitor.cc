dc1b05dc4ee95413fd9e3e504cc6fce4b997481a
22051240-Hu Botao <794056034@qq.com>
2024-04-14 12:20:18 +0800

    add coap.h

---
+++ example/monitor.cc
@@ -0,0 +1,13 @@
+#include "coap.h"
+
+struct sensor_t {
+};
+
+class MyServer: public COAPServer {
+public:
+
+};
+
+int main(int argc, char** argv) {
+
+}
---
+++ include/coap.h
@@ -0,0 +1,10 @@
+#ifndef __COAP_SERVER_COAP__
+#define __COAP_SERVER_COAP__
+
+#include "Args.h"
+#include "Log.h"
+#include "config.h"
+#include "CoAPServer.h"
+#include "CoAPMessage.h"
+
+#endif
