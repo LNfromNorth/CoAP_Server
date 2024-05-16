ac1afe0505713c9590027d4b50b8bc6fefdd407f
hubotao <lnfromnorth@qq.com>
2024-05-16 08:37:27 +0800

    finish the test of coap message

--- tests/test_msg.cc
+++ tests/test_msg.cc
@@ -6,10 +6,32 @@
 int main() {
     std::vector<uint8_t> buffer = {0x40, 0x01, 0x12, 0x34, 0x45, 0x63, 0x81, 0x65, 0x6c, 0x65, 0xff, 0x6d, 0x6f, 0x6e, 0x21, 0x77, 0x6f, 0x72, 0x6c, 0x64};
     COAPMessage coap_message;
+    printf("--------- message in --------------\n");
+    for(int i = 0; i < buffer.size(); i++) {
+        printf("%02x ", buffer[i]);
+    }
+    printf("\n");
+    printf("--------- test parse --------------\n");
     if(coap_message.parse(buffer)) {
         coap_message.print();
     } else {
         std::cout << "error" << std::endl;
     }
+    printf("--------- test func --------------\n");
+    printf("is ack : %d\n", coap_message.isACK());
+    printf("is con : %d\n", coap_message.isCon());
+    printf("--------- test format ------------\n");
+    char* format = coap_message.format();
+    for(int i = 0; i < coap_message.get_size(); i++)
+        printf("%02x ", (uint8_t)format[i]);
+    printf("\n");
+    printf("--------- test match -------------\n");
+    for(int i = 0; i < sizeof(format); i++) {
+        if((uint8_t)buffer[i] != (uint8_t)format[i])  {
+            printf("not match!\n");
+            return 0;
+        }
+    }
+    printf("match!\n");
     return 0;
 }
